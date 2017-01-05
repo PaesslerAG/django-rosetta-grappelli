7f4abb120d5b2b05722b49360581629a6b84ff2b
Martin Bauer <martin.bauer@paessler.com>
2017-01-05 15:26:55 +0100

    prepare new pypi release after github transfer

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-rosetta-grappelli",
-    version = "1.1",
-    description = """
+    name="django-rosetta-grappelli2",
+    version="1.1",
+    description="""
     Compatibility templates for django rosetta when using django-grappelli
     """,
-    author = "Platypus Creation",
-    author_email = "contact@platypus-creation.com",
-    url = "https://github.com/platypus-creation/django-rosetta-grappelli",
-    packages = find_packages(),
+    author="Platypus Creation",
+    author_email="contact@platypus-creation.com",
+    url="https://github.com/belugame/django-rosetta-grappelli",
+    packages=find_packages(),
     install_requires=[
         'django-rosetta>=0.6.5',
     ],
