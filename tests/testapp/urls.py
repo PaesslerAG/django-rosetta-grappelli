4cae72162a25ed3af2c1ac8badb6610fabf077c6
Peter <peter.zsoldos@gmail.com>
2017-06-15 15:27:03 +0200

    add testview to easily get rendered templates
    path is the template name, querystring the context
    
    doing this instead of using render_to_string 'coz IIRC
    that changed a lot over the django versions and don't yet
    know which versions are compatible here

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,7 +1,11 @@
-# from django.conf.urls import url, include
+from django.conf.urls import url, include
+from django.contrib import admin
+from testapp.views import PathIsTemplateNameQueryStringContextView
 
 
 urlpatterns = [
-    # url(r'^admin/', include(admin.site.urls), {}, "admin-index"),
-    # url(r'^some-path/$', some_view, {}, 'some_view'),
+    url(r'^admin/', include(admin.site.urls), {}, "admin-index"),
+    url(r'^rosetta/', include('rosetta.urls')),
+    url(r'^(?P<template_name>.*)$',
+        PathIsTemplateNameQueryStringContextView.as_view()),
 ]
