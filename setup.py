9427cf03c993a64851c243df6e214ad71958917c
Peter <peter.zsoldos@gmail.com>
2017-06-15 14:43:30 +0200

    prefactoring: get version from a file

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('VERSION', 'r') as f:
+    version = f.read().strip()
+
 setup(
     name="django-rosetta-grappelli2",
-    version="1.1",
+    version=version,
     description="""
     Compatibility templates for django rosetta when using django-grappelli. Continued
     development from the original but stalled django-rosetta-grappelli project.
